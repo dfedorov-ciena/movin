5b024264dbaaceb6304303bcbc984a1c4adf52fb
Dmitri Fedorov <34586085+dfedorov-ciena@users.noreply.github.com>
2020-07-24 14:52:39 -0700

    updated

--- infras/esxi-one/main.tf
+++ infras/esxi-one/main.tf
@@ -6,17 +6,43 @@ data "vsphere_datacenter" "dc" {
   name = "ha-datacenter"
 }
 
-data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+data "vsphere_datastore" "ds" {
+  name = "datastore1"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "cluster1"
+data "vsphere_resource_pool" "rp" {
+  name = "cluester1"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_network" "network" {
-  name          = "vmnet100"
+data "vsphere_network" "vmnet100" {
+  name = "vmnet100"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+data "vsphere_network" "vmnet1000" {
+  name = "vmnet1000"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name = "terraform-test"
+  resource_pool_id = data.vsphere_resource_pool.rp.id
+  datastore_id = data.vsphere_datastore.ds.id
+  num_cpus = 8
+  memory = 1024
+  guest_id = "Debian Linux"
+  network_interface {
+    network_id = data.vsphere_network.vmnet100.id
+  }
+  network_interface {
+    network_id = data.vsphere_network.vmnet1000.id
+  }
+  disk {
+    label = "disk"
+    size = 20
+  }
+}
+
+
